45fde70b582452b8a4271ded4106b18b060f17bd
Keven Martins <kevencardoso1@hotmail.com>
2022-10-03 11:48:22 -0300

    finalizacao commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Projeto_MVC_Dio_Pottencial.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
